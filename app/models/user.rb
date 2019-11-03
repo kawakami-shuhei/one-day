9265c365688ba88b8cc3032bececefd66b79e4bf
kawakamishuhei <shuhei.k.1127.96@ezweb.ne.jp>
2019-11-03 14:28:43 +0900

    one-day

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # has_many :cards
-  # has_many :contents
+  has_many :cards
+  has_many :contents
 end
