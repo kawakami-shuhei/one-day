ea8522b0be466278fff8fe8e4db0f9fb2637097b
kawakamishuhei <shuhei.k.1127.96@ezweb.ne.jp>
2019-11-06 16:57:13 +0900

    one-day

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,9 +1,77 @@
 $(function() {
-  $('.header').click(function(){
-    console.log('DADA')
-  })
-
   $(".task-content__list-box__div-card").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
+    
+  }, function() {
+    $(this).css("background", "");
+  });
+
+  $(".task-content__list-add").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
+    
+  }, function() {
+    $(this).css("background", "");
+  });
+
+  $(".card-edit__box").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
+    
+  }, function() {
+    $(this).css("background", "");
+  });
+
+  $(".card-delete__box").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
+    
+  }, function() {
+    $(this).css("background", "");
+  });
+
+  $(".card-button").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
+    
+  }, function() {
+    $(this).css("background", "");
+  });
+
+  $(".card-add").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
+    
+  }, function() {
+    $(this).css("background", "");
+  });
+
+  $(".log-out").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
+    
+  }, function() {
+    $(this).css("background", "");
+  });
+
+  $(".log-in").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
+    
+  }, function() {
+    $(this).css("background", "");
+  });
+
+  $(".new-user").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
+    
+  }, function() {
+    $(this).css("background", "");
+  });
+
+  $(".submit").hover(function() {
     console.log(this)
     $(this).css("background", "rgb(224, 224, 224)");
     
