4dc45956ae3cbb99fea13d9f9912d36068584376
kawakamishuhei <shuhei.k.1127.96@ezweb.ne.jp>
2019-11-10 12:04:29 +0900

    one-day

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,81 +1,81 @@
-// $(function() {
-//   $(".task-content__list-box__div-card").hover(function() {
-//     console.log(this)
-//     $(this).css("background", "rgb(224, 224, 224)");
+$(function() {
+  $(".task-content__list-box__div-card").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
     
-//   }, function() {
-//     $(this).css("background", "");
-//   });
+  }, function() {
+    $(this).css("background", "");
+  });
 
-//   $(".task-content__list-add").hover(function() {
-//     console.log(this)
-//     $(this).css("background", "rgb(224, 224, 224)");
+  $(".task-content__list-add").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
     
-//   }, function() {
-//     $(this).css("background", "");
-//   });
+  }, function() {
+    $(this).css("background", "");
+  });
 
-//   $(".card-edit__box").hover(function() {
-//     console.log(this)
-//     $(this).css("background", "rgb(224, 224, 224)");
+  $(".card-edit__box").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
     
-//   }, function() {
-//     $(this).css("background", "");
-//   });
+  }, function() {
+    $(this).css("background", "");
+  });
 
-//   $(".card-delete__box").hover(function() {
-//     console.log(this)
-//     $(this).css("background", "rgb(224, 224, 224)");
+  $(".card-delete__box").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
     
-//   }, function() {
-//     $(this).css("background", "");
-//   });
+  }, function() {
+    $(this).css("background", "");
+  });
 
-//   $(".card-button").hover(function() {
-//     console.log(this)
-//     $(this).css("background", "rgb(224, 224, 224)");
+  $(".card-button").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
     
-//   }, function() {
-//     $(this).css("background", "");
-//   });
+  }, function() {
+    $(this).css("background", "");
+  });
 
-//   $(".card-add").hover(function() {
-//     console.log(this)
-//     $(this).css("background", "rgb(224, 224, 224)");
+  $(".card-add").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
     
-//   }, function() {
-//     $(this).css("background", "");
-//   });
+  }, function() {
+    $(this).css("background", "");
+  });
 
-//   $(".log-out").hover(function() {
-//     console.log(this)
-//     $(this).css("background", "rgb(224, 224, 224)");
+  $(".log-out").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
     
-//   }, function() {
-//     $(this).css("background", "");
-//   });
+  }, function() {
+    $(this).css("background", "");
+  });
 
-//   $(".log-in").hover(function() {
-//     console.log(this)
-//     $(this).css("background", "rgb(224, 224, 224)");
+  $(".log-in").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
     
-//   }, function() {
-//     $(this).css("background", "");
-//   });
+  }, function() {
+    $(this).css("background", "");
+  });
 
-//   $(".new-user").hover(function() {
-//     console.log(this)
-//     $(this).css("background", "rgb(224, 224, 224)");
+  $(".new-user").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
     
-//   }, function() {
-//     $(this).css("background", "");
-//   });
+  }, function() {
+    $(this).css("background", "");
+  });
 
-//   $(".submit").hover(function() {
-//     console.log(this)
-//     $(this).css("background", "rgb(224, 224, 224)");
+  $(".submit").hover(function() {
+    console.log(this)
+    $(this).css("background", "rgb(224, 224, 224)");
     
-//   }, function() {
-//     $(this).css("background", "");
-//   });
-// });
+  }, function() {
+    $(this).css("background", "");
+  });
+});
